99bd0e6607148082b8257b8bca3722dc69c56c67
ly6183 <ly6183@ly6183s-mbp.fios-router.home>
2019-01-28 00:19:53 -0500

    add management class for declare input code

---
+++ src/javaprojects/MembershipManagement.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaprojects;
+import java.util.InputMismatchException;
+import java.util.LinkedList;
+import java.util.Scanner;
+/**
+ *
+ * @author ly6183
+ */
+public class MembershipManagement {
+ 
+    
+    final private Scanner reader = new Scanner(System.in);
+    
+    
+    
+}
