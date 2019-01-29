af0cedb73a1d945b05764598dc9854afe52f44ae
ly6183 <ly6183@ly6183s-mbp.fios-router.home>
2019-01-28 22:15:40 -0500

    add management class for declare input code

---
+++ src/javaprojects/Calculator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaprojects;
+
+/**
+ *
+ * @author ly6183
+ */                        //               //
+public interface Calculator <T extends Number>
+{
+ 
+//
+    double calculateFees(T ClubID);
+    //
+    
+}
