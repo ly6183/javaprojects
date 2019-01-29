4f94b3e2824dc1a9affbbea2a50f18b7ffba1e9f
ly6183 <ly6183@ly6183s-mbp.fios-router.home>
2019-01-28 22:13:37 -0500

    add management class for declare input code

---
+++ src/javaprojects/JavaProjects.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaprojects;
+import java.util.LinkedList;
+/**
+ *
+ * @author li.yu
+ */
+public class JavaProjects {
+
+    public static void main(String[] args) {
+      String mem;
+      
+      MembershipManagement mm = new MembershipManagement();
+      FileHandler fh = new FileHandler();
+      
+      LinkedList<Member> members = fh.readFile();
+      int choice = mm.getChoice();
+      
+      while (choice !=-1)
+      {
+          switch(choice){
+              case 1:
+                  mem = mm.addMembers(members);
+                  fh.appendFile(mem);
+                  break;
+              case 2:
+                  mm.removeMember(members);
+                  fh.overwriteFile(members);
+                  break;
+              case 3:
+                  mm.printMemberInfo(members);
+                  break;
+              default:
+                  System.out.print("\nYou have selected an invalid option.\n\n");
+                  break;
+      }
+          choice = mm.getChoice();
+      }
+      System.out.println("\nGoodBye");
+    }
+    
+}
